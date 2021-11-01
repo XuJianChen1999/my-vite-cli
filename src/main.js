cda679c0e0f8f2eb041470ec7c918f38f96e1310
徐建辰 <2949742899@qq.com>
2021-11-01 15:20:36 +0800

    按需引入组件

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import {createApp} from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-// import ElementPlus from './plugins/element-plus'
-import ElementPlus from 'element-plus'
+import ElementPlus from './plugins/element-plus'
 
 const app = createApp(App)
 app.use(ElementPlus).use(router).use(store).mount('#app')
